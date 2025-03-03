4caabbd3ba7275e486f760269414f4c9866dc28a
Takaya Ono <tagat92tagat@gmail.com>
2025-03-03 16:26:12 +0900

    background.tsの動作なくす

--- src/background.ts
+++ src/background.ts
@@ -1,9 +1,4 @@
 chrome.runtime.onInstalled.addListener(() => {
     const extensionId = chrome.runtime.id; // 拡張機能のIDを取得
     console.log("拡張ID:", extensionId);
-  
-    // 新しいタブを開く例
-    chrome.tabs.create({
-      url: `chrome-extension://${extensionId}/index.html`
-    });
   });
